af8a6d6d2bff35ea787cf674d322cf08a577626e
Martim <martimdantaslima@gmail.com>
2024-07-05 13:08:32 +0100

    Dia 5 de julho tou no episódio 10

--- routes/web.php
+++ routes/web.php
@@ -1,47 +1,23 @@
 <?php
-use \Illuminate\Support\Arr;
+
 use Illuminate\Support\Facades\Route;
-
-class Job {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [ 
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ]
-            ];
-    } 
-}
-
-
-
+use App\Models\Job;
 
 Route::get('/', function () {
-    return view('home');
+    $jobs = Job::all();
+    dd($jobs);
+    //  return view('home');
 });
+
+
 Route::get('/jobs', function ()  {
     return view('jobs', [
         'jobs' => Job::all()
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) { 
-            
-        $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
-
-        dd($job);
+Route::get('/jobs/{id}', function ($id) {
+    $job = Job::find($id);
 
     return view('job', ['job' => $job]);
 });
